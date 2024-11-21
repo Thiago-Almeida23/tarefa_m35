a103f4a0941e72d66d15ff8cd142b80a224771b0
Thiago-Almeida23 <thiagocldealmeida@gmail.com>
2024-11-21 10:09:52 -0300

    Substituindo JDBC para JPA no projeto de Vendas.

---
+++ Tarefa_M35/src/dao/jpa/IClienteJpaDAO.java
@@ -0,0 +1,8 @@
+package dao.jpa;
+
+import dao.generic.jpa.IGenericJpaDAO;
+import domain.jpa.ClienteJpa;
+
+public interface IClienteJpaDAO extends IGenericJpaDAO<ClienteJpa, Long>{
+
+}
---
+++ Tarefa_M35/src/dao/jpa/IProdutoJpaDAO.java
@@ -0,0 +1,8 @@
+package dao.jpa;
+
+import dao.generic.jpa.IGenericJpaDAO;
+import domain.jpa.ProdutoJpa;
+
+public interface IProdutoJpaDAO extends IGenericJpaDAO<ProdutoJpa, Long>{
+
+}
